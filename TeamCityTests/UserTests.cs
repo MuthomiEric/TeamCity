676ab11fbd39548032712e5919c04e367f13d62d
Muthomi <muthomi102@gmail.com>
2020-06-01 12:08:12 +0300

    second

---
+++ TeamCityTests/UserTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace TeamCityTests
+{
+    public class UserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Assert_That_A_String_Equals_Name()
+        {
+            string name = "Muthomi";
+
+            Assert.AreEqual("Muthomi", name);
+        }
+    }
+}
